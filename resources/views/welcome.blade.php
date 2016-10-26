94a556b0607aa9563694d10c242d80dd283f5edd
t-hebert73 <t.hebert73@gmail.com>
2016-10-26 00:22:31 -0400

    add js hide of mail subscribe thing on mobile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -492,7 +492,52 @@
                     });
 
         });
+
+        // Hide Header on on scroll down
+        var didScroll;
+        var lastScrollTop = 0;
+        var delta = 5;
+        var navbarHeight = $('.navbar-header').outerHeight();
+
+        $(window).scroll(function(event){
+            didScroll = true;
+        });
+
+        setInterval(function() {
+            if (didScroll) {
+                hasScrolled();
+                didScroll = false;
+            }
+        }, 250);
+
+        function hasScrolled() {
+            var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+            if(width < 767) {
+                var st = $(this).scrollTop();
+
+                // Make sure they scroll more than delta
+                if(Math.abs(lastScrollTop - st) <= delta)
+                    return;
+
+                // If they scrolled down and are past the navbar, add class .nav-up.
+                // This is necessary so you never see what is "behind" the navbar.
+                if (st > lastScrollTop && st > navbarHeight){
+                    // Scroll Down
+                    $('#mc_embed_signup').removeClass('show').addClass('hide');
+                } else {
+                    // Scroll Up
+                    if(st + $(window).height() < $(document).height()) {
+                        $('#mc_embed_signup').removeClass('hide').addClass('show');
+                    }
+                }
+
+                lastScrollTop = st;
+            }
+
+        }
     });
+
 </script>
 <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
